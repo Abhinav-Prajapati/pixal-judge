41b494355b5dbd778acd670ff8f58dc37ba9d2db
Abhinav <abhinavprajapati2001@gmail.com>
2025-11-10 00:33:11 +0530

    🧹 Clean up comments in database.py

--- server/database.py
+++ server/database.py
@@ -6,10 +6,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from config import DATABASE_URL, DB_SCHEMA
 
-# Define Base here to avoid circular imports
 Base = declarative_base()
 
-# The connect_args option sets the default search path to our custom schema.
 engine = create_engine(
     DATABASE_URL,
     connect_args={'options': f'-csearch_path={DB_SCHEMA}'}
