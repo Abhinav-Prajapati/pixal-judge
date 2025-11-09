e8c171f030296520ad56f76dfbc0be1072911945
Abhinav <abhinavprajapati2001@gmail.com>
2025-11-10 00:34:00 +0530

    ♻️ Refactor image utils to use Celery .delay() instead of BackgroundTasks

--- server/src/images/utils.py
+++ server/src/images/utils.py
@@ -2,7 +2,6 @@
 Utility functions for Images domain.
 """
 from pathlib import Path
-from fastapi import BackgroundTasks
 from fastapi.responses import FileResponse
 from typing import List
 
@@ -26,39 +25,9 @@ def get_file_response(file_path: Path, resource_name: str = "File") -> FileRespo
     return FileResponse(file_path)
 
 
-def queue_image_processing(
-    background_tasks: BackgroundTasks,
-    image_id: int,
-    thumbnail_task,
-    embedding_task
-):
-    """
-    Queue background tasks for processing a single image.
-    
-    Args:
-        background_tasks: FastAPI background tasks
-        image_id: ID of the image to process
-        thumbnail_task: Thumbnail generation function
-        embedding_task: Embedding generation function
-    """
-    background_tasks.add_task(thumbnail_task, image_id)
-    background_tasks.add_task(embedding_task, image_id)
-
-
-def queue_multiple_image_processing(
-    background_tasks: BackgroundTasks,
-    images: List[Image],
-    thumbnail_task,
-    embedding_task
-):
-    """
-    Queue background tasks for processing multiple images.
-    
-    Args:
-        background_tasks: FastAPI background tasks
-        images: List of Image objects to process
-        thumbnail_task: Thumbnail generation function
-        embedding_task: Embedding generation function
-    """
+def queue_image_tasks(images: List[Image], thumbnail_task, embedding_task):
+    """Queue Celery tasks for processing multiple images."""
     for image in images:
-        queue_image_processing(background_tasks, image.id, thumbnail_task, embedding_task)
+        thumbnail_task.delay(image.id)
+        embedding_task.delay(image.id)
+
